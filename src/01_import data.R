86b82991b1fad41734962bcebf2f2c8d02898d56
Dominic Henry <dominichenry@gmail.com>
2021-10-06 14:24:27 -0400

    Database query import fix

--- src/01_import data.R
+++ src/01_import data.R
@@ -1,4 +1,50 @@
-# Import steps moved to server.
+# Import data -------------------------------------------------------------
+library(shiny)
+library(tidyverse)
+library(readxl)
+library(shinythemes)
+library(glue)
+library(shinydashboard)
+library(DT)
+library(shinyBS)
+library(RPostgres)
+library(shinyjs)
+library(DBI)
+
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = "poisondb",
+                      host = "ewtcsusrv01.postgres.database.azure.com",
+                      port = 5432,
+                      user = "awpd_viewer@ewtcsusrv01",
+                      password = "awpd6847#"
+)
+
+## List tables in database
+dbListTables(con)
+
+wp_data <- dbGetQuery(con, 'SELECT * FROM awpd_data_for_r') %>%
+  as_tibble %>%
+  janitor::clean_names() %>%
+  rename(poison_reason = reason_for_p) %>%
+  mutate(tag = ifelse(is.na(tag),"ALL",tag)) %>%
+  mutate(vernacularname = str_trim(vernacularname)) %>%
+  mutate(poison_family = ifelse(is.na(poison_family), "Unknown", poison_family))
+
+
+wp_data <- wp_data %>%
+  mutate(taxa = case_when(is.na(taxa) ~ "Unknown",
+                          TRUE ~ as.character(taxa)))
+## Remove 'zero' dates
+wp_data <- wp_data %>%
+  filter(year > 1950)
+
+(list_countries <- sort(unique(wp_data$country)))
+(list_poisons <- sort(unique(wp_data$poison_family)))
+(list_species <- sort(unique(wp_data$vernacularname)))
+(list_reason <- sort(unique(wp_data$poison_reason)))
+(list_year_start <- as.integer(min(wp_data$year, na.rm = TRUE)))
+(list_year_end <- as.integer(max(wp_data$year, na.rm = TRUE)))
+(list_taxa <- sort(unique(wp_data$taxa)))
 
 # Check data --------------------------------------------------------------
 
